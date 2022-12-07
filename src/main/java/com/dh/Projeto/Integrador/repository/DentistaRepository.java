771468341c2daa6adaf83c9c3af69e7dcca538e1
fmtoniolo <9815290@gmail.com>
2022-12-06 22:06:37 -0300

    adicionando Dentista

---
+++ src/main/java/com/dh/Projeto/Integrador/repository/DentistaRepository.java
@@ -0,0 +1,10 @@
+package com.dh.Projeto.Integrador.repository;
+
+import com.dh.Projeto.Integrador.model.Dentista;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DentistaRepository extends JpaRepository<Dentista, Integer> {
+
+}
