8f9f0f6d29288f7fa1a115f303b0ad4082963115
fmtoniolo <9815290@gmail.com>
2022-12-08 18:49:53 -0300

    Atualizando métodos de ConsultaService

--- src/main/java/com/dh/Projeto/Integrador/service/ConsultaService.java
+++ src/main/java/com/dh/Projeto/Integrador/service/ConsultaService.java
@@ -1,25 +1,44 @@
 package com.dh.Projeto.Integrador.service;
 
+import com.dh.Projeto.Integrador.exceptions.ResourceNotFoundException;
 import com.dh.Projeto.Integrador.model.Consulta;
 import com.dh.Projeto.Integrador.model.Dentista;
 import com.dh.Projeto.Integrador.model.Usuario;
 import com.dh.Projeto.Integrador.repository.ConsultaRepository;
+import com.dh.Projeto.Integrador.Repository.DentistaRepository;
+import com.dh.Projeto.Integrador.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ConsultaService {
     @Autowired
     private final ConsultaRepository consultaRepository;
 
+    @Autowired
+    DentistaRepository dentistaRepository;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+
     public ConsultaService(ConsultaRepository consultaRepository) {
         this.consultaRepository = consultaRepository;
     }
 
-    public Consulta salvar(Consulta consulta) {
+    public Consulta salvar(Consulta consulta) throws ResourceNotFoundException {
+        Dentista dentista = consulta.getDentista();
+        Optional<Dentista> idDentista = dentistaRepository.findById(dentista.getId());
+        Usuario usuario = consulta.getUsuario();
+        Optional<Usuario> idUsuario = usuarioRepository.findById(usuario.getId());
+
+        if(idDentista.isEmpty() || idUsuario.isEmpty()) {
+            throw new ResourceNotFoundException("Usuário e/ou dentista não encontrado(s).");
+        }
         return consultaRepository.save(consulta);
     }
 
